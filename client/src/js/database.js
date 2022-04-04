d194c82cbaa47313a7ff76801201c2d728e35c85
Cleland <cleland@Clelands-MacBook-Pro.local>
2022-04-04 07:48:18 -0400

    Added additional comments to the indexdb and changed the name from the default starter code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,23 +1,23 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('txtEditDb', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('txtEditDb')) {
+        console.log('txtEditDb database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('txtEditDb', { keyPath: 'id', autoIncrement: true });
+      console.log('txtEditDb database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// allows the information to be added to the database, 
 export const putDb = async (id, content) => {
   console.log('PUT to the database');
-  const textEditorDB = await openDB('todos', 1);
-  const transaction = textEditorDB.transaction('todos', 'readwrite');
-  const store = transaction.objectStore('todos');
+  const textEditorDB = await openDB('txtEditDb', 1);
+  const transaction = textEditorDB.transaction('txtEditDb', 'readwrite');
+  const store = transaction.objectStore('txtEditDb');
   const req = store.put({ id: id, todo: content }, id);
   const res = await req;
   console.log('Content Has been Stored', res);
@@ -26,9 +26,9 @@ export const putDb = async (id, content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getAllDb = async () => {
   console.log('GET all from the database');
-  const textEditorDB = await openDB('jate', 1);
-  const transaction = textEditorDB.transaction('jate', 'readonly');
-  const store = transaction.objectStore('jate');
+  const textEditorDB = await openDB('txtEditDb', 1);
+  const transaction = textEditorDB.transaction('txtEditDb', 'readonly');
+  const store = transaction.objectStore('txtEditDb');
   const req = store.getAll();
   const res = await req;
   console.log('res.value', res);
